15cbb8ed0f711337cd4f074b22bd417f69d9894a
gabsrethink <gabriel.melo@rethink.dev>
2022-05-24 01:02:52 -0300

    fix: game over score

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,13 +1,16 @@
 import './GameOver.css'
 import { Link } from "react-router-dom";
+import {useContext} from "react";
+import { ScoreContext } from "../context/ScoreContext";
 
 
 const GameOver = ({score}) => {
+  const {counter} = useContext(ScoreContext);
   return (
     <div>
       <div>
         <h1>Fim de Jogo!</h1>
-        <h2>A sua pontuação foi: <span>{score}</span></h2>
+        <h2>A sua pontuação foi: <span>{counter}</span></h2>
         <Link to="/" className="button">
         Página inicial
       </Link>
